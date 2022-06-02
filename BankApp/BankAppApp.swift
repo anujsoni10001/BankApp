1d1d5321df765057e20847413217b78e1862b7d8
Anuj Soni <48941274+anujsoni10001@users.noreply.github.com>
2022-06-02 09:46:27 +0530

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Anuj Soni on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
